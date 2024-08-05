2f17cb77639eef5e5b1606578e252c0cb6d213d8
decyy <69968985+decyy@users.noreply.github.com>
2024-08-05 19:56:41 +0200

    Add files via upload

---
+++ colors/custom_theme.lua
@@ -0,0 +1,1 @@
+require("custom_theme").setup({})
---
+++ lua/custom_theme/palette.lua
@@ -0,0 +1,17 @@
+local colors = {
+  bg = "#040405",
+  fg = "#a6accd",
+  gray = "#767c9d",
+  blue = "#add7ff",
+  cyan = "#89ddff",
+  green = "#5de4c7",
+  magenta = "#f087bd",
+  red = "#d0679d",
+  yellow = "#fffac2",
+  orange = "#5fb3a1",
+  purple = "#91b4d5",
+  dark_bg = "#101315",
+  light_bg = "#303340",
+}
+
+return colors
